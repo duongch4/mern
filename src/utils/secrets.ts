48581e695d0ef5ecfd8ac53fbc45c219c5397ba8
Bang Chi Duong <chibangduong1993@gmail.com>
2019-07-08 23:12:07 -0700

    Fix .env

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -2,18 +2,17 @@ import logger from "./logger";
 import * as dotenv from "dotenv";
 import * as fs from "fs";
 
+// Setup multiple .*.env files
 if (fs.existsSync(".env")) {
     logger.debug("Using .env file to supply config environment variables");
     dotenv.config({ path: ".env" });
 } else {
     logger.debug("Using .env.example file to supply config environment variables");
-    dotenv.config({ path: ".env.example" });  // you can delete this after you create your own .env file!
+    dotenv.config({ path: ".env.example" });
 }
 
-const isProd = process.env.NODE_ENV === "production"; // Anything else is treated as 'dev'
-
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
-export const MONGODB_URI = isProd ? process.env["MONGODB_URI"] : process.env["MONGODB_URI_LOCAL"];
+export const MONGODB_URI = process.env["MONGODB_URI"];
 
 if (!SESSION_SECRET) {
     logger.error("No client secret => Require Setting SESSION_SECRET environment variable.");
@@ -21,10 +20,6 @@ if (!SESSION_SECRET) {
 }
 
 if (!MONGODB_URI) {
-    if (isProd) {
-        logger.error("No mongo connection string => Require Setting MONGODB_URI environment variable.");
-    } else {
-        logger.error("No mongo connection string => Require Setting MONGODB_URI_LOCAL environment variable.");
-    }
+    logger.error("No mongo connection string => Require Setting MONGODB_URI environment variable.");
     process.exit(1);
 }
