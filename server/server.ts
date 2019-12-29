4b6eb8206f6cb513570e9aca469c6390bc6ce78c
Bang Chi Duong <chibangduong1993@gmail.com>
2019-12-28 21:11:44 -0500

    HMR server without re-starting the whole server

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,8 @@
-import http from "http";
 import { ExpressServer } from "./ExpressServer";
 
 const port = process.env.PORT || "3000";
-let server: http.Server = new ExpressServer().listen(port);
+new ExpressServer().listen(port);
 
 if (module.hot) {
-    module.hot.accept("./ExpressServer", () => {
-        server.close();
-        server = new ExpressServer().listen(port);
-    });
+    module.hot.accept("./ExpressServer");
 }
