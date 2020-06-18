064fdd6ad1d95f4d63f61ad3dd13abcd100da905
Bang Chi Duong <chibangduong1993@gmail.com>
2020-06-18 03:22:40 -0700

    Cleaned up ProfileInfo

--- client/components/account/ProfileInfo.tsx
+++ client/components/account/ProfileInfo.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { UserPayload } from "../../models/User";
 
-type ProfileInfoProps = {
-    currUser: UserPayload;
+import { useUserAuthenticated } from "../../context/UserContext";
+import { JsonPrint } from "../../components/utils/JsonPrint";
+
+export const ProfileInfo = () => {
+    const user = useUserAuthenticated().state;
+    return (
+        <div id="profile-info">
+            <h1>Only Authenticated Users See Their Info Here!</h1>
+            <h2>Hello: {user.email}</h2>
+            <JsonPrint data={user} />
+        </div>
+    );
 };
-
-export const ProfileInfo = (props: ProfileInfoProps) => (
-    <div id="profile-info">
-        <h1>Only Authenticated Users See Their Info Here!</h1>
-        <h2>Hello: {props.currUser.email}</h2>
-    </div>
-);
