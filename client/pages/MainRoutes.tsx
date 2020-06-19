4453b8013bf2794aef829cde8cc730e324e93fea
Bang Chi Duong <chibangduong1993@gmail.com>
2020-06-19 06:23:25 -0700

    Added SpinnerPage

--- client/pages/MainRoutes.tsx
+++ client/pages/MainRoutes.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 
 import { useUserCheck } from "../contexts/UserContext";
-import { StatusPage } from "./status/StatusPage";
-
-import Log from "../utils/Log";
+import { FullPageSpinner } from "./spinner/SpinnerPage";
 
 const AuthenticatedRoutes = React.lazy(() => import("./AuthenticatedRoutes"));
 const UnauthenticatedRoutes = React.lazy(() => import("./UnauthenticatedRoutes"));
 
 export const MainRoutes = () => {
     const user = useUserCheck()?.state;
-    Log.info("In MainRoutes");
-    Log.info(user);
-
-    // TODO: FALL BACK to LOADING/SPINNER
     return (
-        <React.Suspense fallback={<StatusPage />}>
+        <React.Suspense fallback={<FullPageSpinner />}>
             {user ? <AuthenticatedRoutes /> : <UnauthenticatedRoutes />}
         </React.Suspense>
     );
