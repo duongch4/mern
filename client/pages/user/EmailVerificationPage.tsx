00ad872fef426dcaf2ca25fc07c3e6d950180e56
Bang Chi Duong <chibangduong1993@gmail.com>
2020-08-10 04:02:12 -0700

    Added EmailVerificationPage

---
+++ client/pages/user/EmailVerificationPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+import querystring from "querystring";
+import { useUserAuthenticated } from "../../contexts/UserContext";
+
+export const EmailVerificationPage = () => {
+    const { user } = useUserAuthenticated();
+    const query = querystring.parse(useLocation().search.replace("?", ""));
+    return (
+        <div id="verified-email-page" className="container-fluid container-main text-center">
+            <div className="pb-2 mt-2 mb-4">
+                <h3>{query.message}</h3>
+                <Link className="btn not-found-link" to={`/users/${user.id}`}>Go To Account Page</Link>
+            </div>
+        </div>
+    );
+};
