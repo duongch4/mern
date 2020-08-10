e367310eb2fca8f972a7898210a603465c5fe32b
Bang Chi Duong <chibangduong1993@gmail.com>
2020-08-10 04:01:50 -0700

    Added emailVerified field; Cleaned up email verification part

--- client/components/form/ProfileForm.tsx
+++ client/components/form/ProfileForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { AjaxHandler } from "../../utils/AjaxHandler";
+import { AjaxHandler, AjaxHandlerAxios } from "../../utils/AjaxHandler";
 import { AlertMessage } from "../utils/AlertMessage";
 import { FormGroupText } from "./FormGroupText";
 import { FormGroupRadio } from "./FormGroupRadio";
@@ -11,6 +11,7 @@ import Log from "../../utils/Log";
 export type ProfileFormStates = {
     message: string;
     email: string;
+    emailVerified: boolean;
     firstName: string;
     lastName: string;
     gender: string;
@@ -20,12 +21,14 @@ export type ProfileFormStates = {
 
 export const ProfileForm = () => {
     const { user, setUser } = useUserAuthenticated();
-    const url = `/api/account/profile/${user.id}`;
+    const urlProfile = `/api/users/${user.id}/profile`;
+    const urlEmailVerification = `/api/users/${user.id}/verify`;
     const textButton = "Update Profile";
 
     const [state, setState] = React.useState({
         message: "",
         email: user.email,
+        emailVerified: user.emailVerified,
         firstName: user.profile.firstName,
         lastName: user.profile.lastName,
         gender: user.profile.gender,
@@ -36,12 +39,13 @@ export const ProfileForm = () => {
     const onInputChange = (field: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
         setState({
             ...state,
+            message: "",
             [field]: event.target.value
         });
     };
 
     const submit = async (): Promise<any> => {
-        Log.trace("Submitting form to: ", url);
+        Log.trace("Submitting form to: ", urlProfile);
         const data = {
             email: state.email,
             firstName: state.firstName,
@@ -52,16 +56,18 @@ export const ProfileForm = () => {
         };
 
         try {
-            const response = await AjaxHandler.putRequest(url, data);
+            const response = await AjaxHandler.putRequest(urlProfile, data);
             Log.trace("YAY!!!");
             Log.trace(response);
             setState({
                 ...state,
+                emailVerified: response.payload.emailVerified,
                 message: response.message
             });
             setUser({
                 ...user,
                 email: state.email,
+                emailVerified: state.emailVerified,
                 profile: {
                     ...user.profile,
                     firstName: state.firstName,
@@ -83,10 +89,20 @@ export const ProfileForm = () => {
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        submit();
+    };
+
+    const verifyEmail = async () => {
         try {
-            submit();
+            const response = await AjaxHandlerAxios.getRequest(urlEmailVerification);
+            Log.info(response);
+            setState({
+                ...state,
+                message: response.message
+            });
         }
         catch (err) {
+            Log.error(err);
             setState({
                 ...state,
                 message: err.message
@@ -97,10 +113,11 @@ export const ProfileForm = () => {
     const EmailVerification = () => (
         <div className="offset-sm-3 col-sm-5 col-md-5 pl-3">
             {
-                state.website
-                    ? <div className="font-italic text-success">Verified</div>
-                    : <div className="font-italic text-danger">
-                        Unverified: &nbsp; <a href="/api/account/verify">Send verification email</a>
+                state.emailVerified ?
+                    <div className="font-italic text-success">Verified</div> :
+                    <div>
+                        <span className="font-italic text-danger">Unverified: </span>
+                        <a className="font-italic text-underline" onClick={verifyEmail}>Send verification email</a>
                     </div>
             }
         </div>
@@ -109,38 +126,37 @@ export const ProfileForm = () => {
     return (
         <form onSubmit={onSubmit}>
             <AlertMessage message={state.message} />
-            <form>
-                <FormGroupText
-                    type={"email"} id={"profile-id-email"} value={state.email}
-                    onChange={onInputChange("email")}
-                    label={"Email"}
-                    smallHelpDiv={EmailVerification()}
-                />
-                <FormGroupText
-                    type={"text"} id={"profile-id-firstName"} value={state.firstName}
-                    onChange={onInputChange("firstName")}
-                    label={"First Name"}
-                />
-                <FormGroupText
-                    type={"text"} id={"profile-id-lastName"} value={state.lastName}
-                    onChange={onInputChange("lastName")}
-                    label={"Last Name"}
-                />
-                <FormGroupText
-                    type={"text"} id={"profile-id-location"} value={state.location}
-                    onChange={onInputChange("location")}
-                    label={"Location"}
-                />
-                <FormGroupText
-                    type={"text"} id={"profile-id-website"} value={state.website}
-                    onChange={onInputChange("website")}
-                    label={"Website"}
-                />
-                <FormGroupRadio
-                    label="Gender" values={["Male", "Female", "Other"]}
-                    currValue={state.gender} onChange={onInputChange("gender")}
-                />
-            </form>
+            <FormGroupText
+                type={"email"} id={"profile-id-email"} value={state.email}
+                onChange={onInputChange("email")}
+                label={"Email"}
+                smallHelpDiv={EmailVerification()}
+                required={true}
+            />
+            <FormGroupText
+                type={"text"} id={"profile-id-firstName"} value={state.firstName}
+                onChange={onInputChange("firstName")}
+                label={"First Name"}
+            />
+            <FormGroupText
+                type={"text"} id={"profile-id-lastName"} value={state.lastName}
+                onChange={onInputChange("lastName")}
+                label={"Last Name"}
+            />
+            <FormGroupText
+                type={"text"} id={"profile-id-location"} value={state.location}
+                onChange={onInputChange("location")}
+                label={"Location"}
+            />
+            <FormGroupText
+                type={"text"} id={"profile-id-website"} value={state.website}
+                onChange={onInputChange("website")}
+                label={"Website"}
+            />
+            <FormGroupRadio
+                label="Gender" values={["Male", "Female", "Other"]}
+                currValue={state.gender} onChange={onInputChange("gender")}
+            />
             <button type="submit" className="btn">{textButton}</button>
         </form>
     );
